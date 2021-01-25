0d07655bacbaf689999bd5fa64bc5706dc5ceb0e
zp.wei <20180710>
2021-01-25 15:48:45 +0800

    feat: 新增下载本地服务器文件

--- src/main/java/org/wzp/oauth2/util/FileUtil.java
+++ src/main/java/org/wzp/oauth2/util/FileUtil.java
@@ -5,9 +5,15 @@ import org.springframework.util.DigestUtils;
 import org.springframework.util.ResourceUtils;
 import org.wzp.oauth2.config.CustomConfig;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @Author: zp.wei
@@ -297,6 +303,43 @@ public class FileUtil {
         }
         log.info("下载成功!!!");
         return "200";
+    }
+
+
+    /**
+     * 从本地服务器下载文件
+     *
+     * @param file
+     * @param response
+     */
+    public static void downloadFile(File file, HttpServletResponse response) {
+        OutputStream os = null;
+        try {
+            // 取得输出流
+            os = response.getOutputStream();
+            String contentType = Files.probeContentType(Paths.get(file.getAbsolutePath()));
+            response.setHeader("Content-Type", contentType);
+            response.setHeader("Content-Disposition", "attachment;filename=" + new String(file.getName().getBytes("utf-8"), "ISO8859-1"));
+            FileInputStream fileInputStream = new FileInputStream(file);
+            WritableByteChannel writableByteChannel = Channels.newChannel(os);
+            FileChannel fileChannel = fileInputStream.getChannel();
+            fileChannel.transferTo(0, fileChannel.size(), writableByteChannel);
+            fileChannel.close();
+            os.flush();
+            writableByteChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //文件的关闭放在finally中
+        finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
