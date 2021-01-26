676355decd14e1baa6013a4c39b71e4617ba2944
nanfengluojin <75766381+nanfengluojin@users.noreply.github.com>
2021-01-26 15:02:28 +0800

    Update RabbitController.java

--- src/main/java/org/wzp/oauth2/controller/back/RabbitController.java
+++ src/main/java/org/wzp/oauth2/controller/back/RabbitController.java
@@ -1,6 +1,5 @@
 package org.wzp.oauth2.controller.back;
 
-import cn.hutool.core.thread.ThreadUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -8,12 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wzp.oauth2.config.BaseConfig;
-import org.wzp.oauth2.util.Result;
 import org.wzp.oauth2.rabbitMq.direct.DirectProducer;
 import org.wzp.oauth2.rabbitMq.fanout.FanoutProducer;
 import org.wzp.oauth2.rabbitMq.simple.SimpleProducer;
 import org.wzp.oauth2.rabbitMq.topic.TopicProducer;
 import org.wzp.oauth2.rabbitMq.work.WorkProducer;
+import org.wzp.oauth2.util.Result;
 
 import javax.annotation.Resource;
 
@@ -42,9 +41,13 @@ public class RabbitController extends BaseConfig {
     @ApiOperation("简单模式")
     @GetMapping("/simple")
     public Result simpleTest() {
-        for (int i = 0; i < 5; i++) {
-            simpleProducer.send();
-            ThreadUtil.sleep(1000);
+        try {
+            for (int i = 0; i < 5; i++) {
+                simpleProducer.send();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return Result.ok();
     }
@@ -53,9 +56,13 @@ public class RabbitController extends BaseConfig {
     @ApiOperation("工作模式")
     @GetMapping("/work")
     public Result workTest() {
-        for (int i = 0; i < 5; i++) {
-            workProducer.send(i);
-            ThreadUtil.sleep(1000);
+        try {
+            for (int i = 0; i < 5; i++) {
+                workProducer.send(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return Result.ok();
     }
@@ -64,10 +71,15 @@ public class RabbitController extends BaseConfig {
     @ApiOperation("发布/订阅模式")
     @GetMapping("/fanout")
     public Result fanoutTest() {
-        for (int i = 0; i < 5; i++) {
-            fanoutProducer.send(i);
-            ThreadUtil.sleep(1000);
+        try {
+            for (int i = 0; i < 5; i++) {
+                fanoutProducer.send(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
         return Result.ok();
     }
 
@@ -75,9 +87,13 @@ public class RabbitController extends BaseConfig {
     @ApiOperation("路由模式")
     @GetMapping("direct")
     public Result directTest() {
-        for (int i = 0; i < 5; i++) {
-            directProducer.send(i);
-            ThreadUtil.sleep(1000);
+        try {
+            for (int i = 0; i < 5; i++) {
+                directProducer.send(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return Result.ok();
     }
@@ -86,9 +102,13 @@ public class RabbitController extends BaseConfig {
     @ApiOperation("通配符模式")
     @GetMapping("/topic")
     public Result topicTest() {
-        for (int i = 0; i < 5; i++) {
-            topicProducer.send(i);
-            ThreadUtil.sleep(1000);
+        try {
+            for (int i = 0; i < 5; i++) {
+                topicProducer.send(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return Result.ok();
     }
